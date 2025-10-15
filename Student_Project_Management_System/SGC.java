db3732367e6dae9f2ea6fcd45a0a8938e004e6e8
thervtechie1797 <rohitv1797@gmail.com>
2025-10-15 20:08:30 +0530

    Save local changes before rebase

---
+++ Student_Project_Management_System/SGC.java
@@ -0,0 +1,7 @@
+package Java_projects.Student_Project_Management_System;
+
+public class SGC {
+public static void main(String[] args) {
+    
+}
+}
--- Student_Project_Management_System/SMS.java
+++
@@ -1,5 +0,0 @@
-package Java_projects.Student_Project_Management System;
-
-public class SMS {
-    
-}
